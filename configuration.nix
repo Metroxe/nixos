68df7a19931a8a6b72a15c1f9b1e5aac41ee725f
Christopher Powroznik <christopher@expopulus.com>
2024-07-23 14:04:05 -0700

    pass cursor image in directly

--- configuration.nix
+++ configuration.nix
@@ -95,9 +95,15 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Fetch the latest cursor AppImage without specifying the hash
-  cursorAppImage = pkgs.fetchurl {
-    url = "https://downloader.cursor.sh/linux/appImage/x64";
+  # Fetch the latest cursor AppImage with specified hash
+  cursorAppImage = pkgs.appimageTools.wrapType2 {
+    pname = "cursor";
+    version = "0.1.0";
+
+    src = pkgs.fetchurl {
+      url = "https://downloader.cursor.sh/linux/appImage/x64";
+      hash = "sha256-Fsy9OVP4vryLHNtcPJf0vQvCuu4NEPDTN2rgXO3Znwo=";
+    };
   };
 
   environment.systemPackages = with pkgs; [
